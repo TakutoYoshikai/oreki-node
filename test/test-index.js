ff4ed909c8c1d3183e267e9e1e5ba6d652b16da1
takuto <takuto.yoshikai@gmail.com>
2020-04-02 14:39:25 +0900

    add test

---
+++ test/test-index.js
@@ -0,0 +1,23 @@
+const test = require("ava");
+const Oreki = require("../index").Oreki;
+test("create Oreki object", t => {
+  const oreki = new Oreki("./test/config-test-correct.json");
+  if (oreki.config === null || oreki.config === undefined) {
+    t.fail();
+    return;
+  }
+  if (oreki.config.database !== "sequelize") {
+    t.fail();
+    return;
+  }
+  t.pass();
+});
+
+test("failing Oreki creation", t => {
+  const oreki = new Oreki("./test/config-test-wrong.json");
+  if (oreki.config !== undefined) {
+    t.fail();
+    return;
+  }
+  t.pass();
+});
