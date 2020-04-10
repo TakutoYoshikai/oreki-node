e7172eefb0b7a2daa8809521cb80893a4284ccaf
EC2 Default User <ec2-user@ip-172-31-38-166.ap-northeast-1.compute.internal>
2020-04-10 04:07:30 +0000

    payment and test

---
+++ payments.js
@@ -0,0 +1,58 @@
+const Sequelize = require("sequelize")
+
+module.exports = function(config) {
+  let sqliteFile = "payment.sqlite3"
+  let test = false
+  if (config !== undefined) {
+    if (config.sqliteFile !== undefined) {
+      sqliteFile = config.sqliteFile
+    }
+    if (config.test !== undefined) {
+      test = config.test
+    }
+  }
+  const sequelize = new Sequelize(
+    "database",
+    "",
+    "",
+    {
+      dialect: "sqlite",
+      storage: sqliteFile
+    }
+  )
+
+  const Payment = sequelize.define(
+    "payment",
+    {
+      address: Sequelize.STRING,
+      user_id: Sequelize.STRING,
+      endpoint: Sequelize.STRING,
+      point: Sequelize.INTEGER,
+      price: Sequelize.DOUBLE
+    },
+    {
+      timestamps: true
+    }
+  )
+
+
+  return {
+    initDB: function() {
+        return sequelize
+          .sync({force: test})
+    },
+    getPayments: function() { 
+      return Payment.findAll()
+    },
+    getPaymentByAddress: function(address) {
+      return Payment.findOne({
+        where: {
+          address: address
+        }
+      })
+    },
+    createPayment: function(obj) {
+      return Payment.create(obj)
+    }
+  }
+}
---
+++ test/test-payment.js
@@ -0,0 +1,49 @@
+const test = require("ava");
+const Payment = require("../payments")({
+  sqliteFile: "test.sqlite3",
+  test: true
+})
+
+test("create payment and get payment", async function(t) {
+  await Payment.initDB()
+  let payment = null
+  try {
+    payment = await Payment.createPayment({
+      address: "address",
+      user_id: "user_id",
+      endpoint: "endpoint",
+      point: 5,
+      price: 1.5
+    })
+  } catch(err) {
+    t.fail(err)
+    return
+  }
+  if (!(payment.address === "address" && payment.user_id === "user_id" && payment.endpoint === "endpoint" && payment.point === 5 && payment.price === 1.5)) {
+    t.fail()
+    return
+  }
+  try {
+    payment = await Payment.getPaymentByAddress("address")
+  } catch(err) {
+    t.fail(err)
+    return
+  }
+  if (payment.address !== "address") {
+    t.fail()
+    return
+  }
+
+  let payments = null
+  try {
+    payments = await Payment.getPayments()
+  } catch(err) {
+    t.fail(err)
+    return
+  }
+  if (!(payments.length === 1 && payments[0].address === "address")) {
+    t.fail()
+    return
+  }
+  t.pass()
+})
