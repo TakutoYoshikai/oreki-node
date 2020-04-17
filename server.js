f48a7a7db5f85bc3419e818eed4f9b55fd4dc050
EC2 Default User <ec2-user@ip-172-31-38-166.ap-northeast-1.compute.internal>
2020-04-17 01:21:28 +0000

    express server

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
+const express = require("express")
+const app = express()
+const Oreki = require("./index").Oreki
+const oreki = new Oreki("config.json")
 
-console.log("oreki server")
+let paymentBuffer = []
+
+app.get("/", function(req, res, next) {
+  res.json({ payments: paymentBuffer })
+  paymentBuffer = []
+})
+
+app.listen(3000, function() {
+  console.log("oreki started")
+})
+
+(async() => {
+  const initialized = await oreki.init()
+  if (!initialized) {
+    console.error("couldn't initialized")
+    return
+  }
+  oreki.on("paid", function(payment) {
+    console.log("PAID")
+    console.log(payment)
+    paymentBuffer.push(payment)
+  })
+  oreki.start()
+  let payment = await oreki.addPayment("user", "endpoint", 5, 1000)
+  console.log(payment.address)
+})()
