4c19c00320414ea3b6255b54e28390eb1980b828
EC2 Default User <ec2-user@ip-172-31-46-231.ap-northeast-1.compute.internal>
2020-06-24 12:19:18 +0000

    comment

--- test/test-index.js
+++ test/test-index.js
@@ -54,6 +54,7 @@ test.serial("add payment", async function (t) {
   t.pass()
 })
 
+/*
 test.serial("check transaction", async function(t) {
   const alice = new Oreki("./test/config-alice.json")
   const bob = new Oreki("./test/config-bob.json")
@@ -115,6 +116,8 @@ test.serial("check transaction", async function(t) {
     t.fail()
   }
 })
+
+*/
 
 test.serial("ethereum transaction check", async function(t) {
   const oreki = new Oreki("./test/config-eth-alice.json")
